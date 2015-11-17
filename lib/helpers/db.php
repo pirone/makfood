ee8e9be48ea2d2cb2920f657080ad152a3bb5049
Bored Elf <shinobies@gmail.com>
2015-11-17 17:50:01 -0200

    Criado helper DB

---
+++ lib/helpers/db.php
@@ -0,0 +1,31 @@
+<?php
+
+class DB {
+
+	private $PDO;
+
+	public function getPDO() {
+		return $this->PDO;
+	}
+
+	public function __construct($host, $user, $pswd, $dbName) {
+		try {
+			$this->PDO = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $user, $pswd);
+			$this->PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_NAMED);
+		} catch (PDOException $e) {
+			die('Não possível conectar ao banco. Motivo: '.$e->getMessage());
+		}
+	}
+
+	public function query($sql, $params = array()) {
+		try {
+			$statement = $this->PDO->prepare($sql);
+			$statement->execute($params);
+			return $statement;
+		} catch (PDOException $e) {
+			die('Erro ao executar a query. Motivo: '.$e->getMessage());
+		}
+	}
+
+}
